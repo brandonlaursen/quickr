b1a579dd4f274962ff3e7a143d71ef40dc627a44
brandon <brandonlaursen95@yahoo.com>
2021-10-06 19:46:11 -0400

    keep noted

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -31,4 +31,12 @@ router.post(
   }),
 );
 
+router.delete(
+  '/',
+  (_req, res) => {
+    res.clearCookie('token');
+    return res.json({ message: 'success' });
+  }
+);
+
 module.exports = router;
