b433fd0e3b11526edb184135e72ad4905517cf6a
brandon <brandonlaursen95@yahoo.com>
2021-10-06 19:35:53 -0400

    tested login

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const asyncHandler = require("express-async-handler");
-const router = express.Router();
 
 const { setTokenCookie, restoreUser } = require("../../utils/auth");
 const { User } = require("../../db/models");
 
+const router = express.Router();
+
+
 // Log in
 router.post(
-  "/",
+  '/',
   asyncHandler(async (req, res, next) => {
     const { credential, password } = req.body;
 
     const user = await User.login({ credential, password });
 
     if (!user) {
-      const err = new Error("Login failed");
+      const err = new Error('Login failed');
       err.status = 401;
-      err.title = "Login failed";
-      err.errors = ["The provided credentials were invalid."];
+      err.title = 'Login failed';
+      err.errors = ['The provided credentials were invalid.'];
       return next(err);
     }
 
@@ -26,7 +28,7 @@ router.post(
     return res.json({
       user,
     });
-  })
+  }),
 );
 
 module.exports = router;
