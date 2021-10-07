aeb91b480a9a7c79b4c41a3266a5d174c2409baa
brandon <brandonlaursen95@yahoo.com>
2021-10-07 14:10:53 -0400

    testing home page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import SignupFormPage from "./components/SignupFormPage";
+
 import * as sessionActions from "./store/session";
+
+import SignupFormPage from "./components/SignupFormPage";
 import Navigation from "./components/Navigation";
+import HomePage from "./components/HomePage";
 
 function App() {
   const dispatch = useDispatch();
@@ -17,6 +20,9 @@ function App() {
       <Navigation isLoaded={isLoaded} />
       {isLoaded && (
         <Switch>
+          <Route>
+            <HomePage />
+          </Route>
           <Route path="/signup">
             <SignupFormPage />
           </Route>
---
+++ frontend/src/components/HomePage/index.js
@@ -0,0 +1,18 @@
+import { useSelector } from 'react-redux';
+import { NavLink, Redirect } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+
+import './HomePage.css'
+
+
+function HomePage() {
+
+  return (
+    <div class="bg_image" >
+        <h1>Welcome to quickr</h1>
+    </div>
+  )
+}
+
+
+export default HomePage;
