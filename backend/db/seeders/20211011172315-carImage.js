3d9cab4851bab428adce2097c88d882e68c8db3b
brandon <brandonlaursen95@yahoo.com>
2021-10-11 13:33:47 -0400

    db changes

--- backend/db/seeders/20211011172315-carImage.js
+++ backend/db/seeders/20211011172315-carImage.js
@@ -11,7 +11,16 @@ module.exports = {
      description: 'The Demon is the work of Dodge’s most hardcore guys who are car enthusiasts of the highest order.',
      createdAt: new Date(),
      updatedAt: new Date(),
-    }
+    },
+    {
+      name: 'MCLAREN 650S SPIDER',
+      userId: 1,
+      showroomId: 1,
+      imageUrl: 'https://s1.cdn.autoevolution.com/images/gallery/MCLAREN-650S-Spyder-5887_38.jpg',
+      description: 'The 650S Can-Am honours the awe-inspiring and fearsome Can-Am race cars that first took to the track in 1966.',
+      createdAt: new Date(),
+      updatedAt: new Date(),
+     }
   ], {});
   },
 
